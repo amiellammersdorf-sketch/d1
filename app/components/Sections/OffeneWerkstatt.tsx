704a3e154c81e1467ec9c21fec168b23a3ff0034
amiellammersdorf-sketch <amiellammersdorf@gmail.com>
2025-11-20 19:06:07 +0100

    Update OffeneWerkstatt.tsx

--- app/components/Sections/OffeneWerkstatt.tsx
+++ app/components/Sections/OffeneWerkstatt.tsx
@@ -2,7 +2,7 @@
 
 export default function OffeneWerkstatt() {
   return (
-    <div className="space-y-6 text-[#021695]">
+    <div className="space-y-4 text-[#021695]">
       {/* 👇 Title hidden on mobile, visible on desktop */}
       <h2 className="hidden md:block text-[26px] md:text-[38px] font-bold tracking-wide">
         OFFENE WERKSTATT
@@ -14,32 +14,22 @@ export default function OffeneWerkstatt() {
 
       <p className="text-[18px] leading-[26px] md:text-2xl md:leading-snug">
         Mit deiner D1 PrintCard erhältst du Zugang zu unserem voll
-        ausgestatteten Siebdruckatelier. Nach einem Einführungskurs kannst Du mit der Karte stundenweise Maschinen nutzen
-        – ganz flexibel, wie mit einer Zehnerkarte für den ÖV.
+        ausgestatteten Siebdruckatelier. Nach einem Einführungskurs kannst Du mit
+        der Karte stundenweise Maschinen nutzen – ganz flexibel, wie mit einer
+        Zehnerkarte für den ÖV.
       </p>
 
       <p className="text-[18px] leading-[26px] md:text-2xl md:leading-snug">
         →{" "}
         <a
-          href="mailto:info@d1studio.ch"
+          href="https://buy.stripe.com/eVq4gB9gv1gzd490DF4Vy00"
+          target="_blank"
+          rel="noopener noreferrer"
           className="hover:font-bold text-[#021695] transition-all duration-200"
         >
           D1 PrintCard bestellen
         </a>
       </p>
-
-      {/* ⭐ Stripe Buy Button */}
-      <div
-        className="mt-8"
-        dangerouslySetInnerHTML={{
-          __html: `
-            <stripe-buy-button
-              buy-button-id="buy_btn_1SVbsARuf2AabxVMW8LYmSbe"
-              publishable-key="pk_live_51SEH5SRuf2AabxVMPVTpbMqGkIoaW4aih9ChvWG7RBoHqkuJ38ytFKSaOGoHMVl3HWM6nuJp0cQRmWQu9Z8lr43900tY63CIxp">
-            </stripe-buy-button>
-          `,
-        }}
-      />
     </div>
   );
 }
