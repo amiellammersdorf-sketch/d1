39344e565485299dc41cfc38a0fe354637301f5f
amiellammersdorf-sketch <amiellammersdorf@gmail.com>
2026-04-12 20:58:09 +0200

    Update sections.tsx

--- app/data/sections.tsx
+++ app/data/sections.tsx
@@ -10,7 +10,6 @@ import Faq from "../components/Sections/Faq";
 
 export const SECTIONS = [
   { id: "siebdruck", label: "SIEBDRUCK", component: <Siebdruck /> },
-  { id: "potm", label: "PRINT OF THE MONTH", component: PrintOfTheMonth },
   { id: "arbeitskleidung-workwear", label: "ARBEITSKLEIDUNG & CORPORATE WEAR", component: <ArbeitskleidungCorporateWear />, },
   { id: "workshops", label: "WORKSHOPS", component: <Workshops /> },
   { id: "events", label: "EVENTS", component: <Events /> },
