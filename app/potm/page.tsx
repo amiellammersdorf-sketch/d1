eff5ec5f7dfadfe7c47b28e6fdb22690d5b55943
amiellammersdorf-sketch <amiellammersdorf@gmail.com>
2026-04-19 18:28:02 +0200

    Set SHOW_POTM to true for POTMPage

--- app/potm/page.tsx
+++ app/potm/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function POTMPage() {
-  const SHOW_POTM = process.env.NEXT_PUBLIC_SHOW_POTM === "true";
+  const SHOW_POTM = true;
 
   if (!SHOW_POTM) {
     return <div className="p-12 text-[#021695]">POTM not live</div>;
