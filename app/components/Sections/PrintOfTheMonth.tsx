c573d039b14472af4d8f10255390bbbb88772a50
amiellammersdorf-sketch <amiellammersdorf@gmail.com>
2026-04-30 13:50:27 +0200

    Update product name in PrintOfTheMonth component

--- app/components/Sections/PrintOfTheMonth.tsx
+++ app/components/Sections/PrintOfTheMonth.tsx
@@ -40,7 +40,7 @@ export default function PrintOfTheMonth() {
       {/* PRODUCT */}
       <div className="space-y-6 text-[18px] leading-[26px] md:text-2xl md:leading-snug">
         <p>
-          <strong>Some are hot and some are not T-Shirt</strong><br />
+          <strong>"Some are hot and some are not" T-Shirt</strong><br />
           CHF 45.–
         </p>
       </div>
