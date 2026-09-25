b7834c74247ba2e937758e83450b9dc567be0210
amiellammersdorf-sketch <amiellammersdorf@gmail.com>
2026-09-25 15:02:34 +0200

    Fix syntax error in image array

--- app/components/Sections/PrintOfTheMonth.tsx
+++ app/components/Sections/PrintOfTheMonth.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const images = [
   "/potm/oct/potm_oct_1.jpg",
   "/potm/oct/potm_oct_2.jpg",
-  "/potm/oct/potm_oct_3.jpg"
+  "/potm/oct/potm_oct_3.jpg",
   "/potm/oct/potm_oct_4.jpg",
 ];
 
